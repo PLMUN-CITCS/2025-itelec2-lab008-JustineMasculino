1580d9cca08fcae4e4788e07e8ba921f71ed354b
Justine Masculino <justinemasculino57@gmail.com>
2025-03-09 19:42:11 +0800

    Submitting Python Week 04 - Laboratory # 08

--- if_statement_basic.py
+++ if_statement_basic.py
@@ -1,6 +1,5 @@
+temperature = 35  # Or any other number
 
-temperature = 40   # Or any other number
+if temperature > 30:
 
-if temperature <= 30:
-
-print("It's a hot day!")  # Indented 4 spacesgit commit -m "Submitting Python Week 04 - Laboratory # 10"
+    print("It's a hot day!")  # Indented 4 spaces
